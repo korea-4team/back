8c9d25854aa892942c62d940d4b53a40e0fc6d12
CJD0402 <wpehd7547@naver.com>
2023-09-24 21:50:47 +0900

    review-board, search

---
+++ src/main/java/com/team/back/dto/ResponseDto.java
@@ -0,0 +1,24 @@
+package com.team.back.dto;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.team.back.common.response.ResponseCode;
+import com.team.back.common.response.ResponseMessage;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResponseDto {
+  private String code;
+  private String message;
+
+  public static ResponseEntity<ResponseDto> databaseError() {
+    ResponseDto result = new ResponseDto(ResponseCode.DATABASE_ERROR, ResponseMessage.DATABASE_ERROR);
+    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(result);
+  }
+}
