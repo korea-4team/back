366f57872a887bdb824c0bb0977b81e9bb34d444
leejs8041 <leejs8041@naver.com>
2023-09-26 16:54:07 +0900

    five

--- src/main/java/com/team/back/service/AdvertisingService.java
+++ src/main/java/com/team/back/service/AdvertisingService.java
@@ -4,27 +4,36 @@ import org.springframework.http.ResponseEntity;
 
 import com.team.back.dto.request.advertisingBoard.PatchAdvertisingRequestDto;
 import com.team.back.dto.request.advertisingBoard.PostAdvertisingRequestDto;
+import com.team.back.dto.response.advertisingBoard.DeleteAdvertisingBoard;
+import com.team.back.dto.response.advertisingBoard.DeleteShortCommentAdvertisingBoard;
+import com.team.back.dto.response.advertisingBoard.GetAdvertisingboard;
+import com.team.back.dto.response.advertisingBoard.GetCurrentAdvertisingBoard;
+import com.team.back.dto.response.advertisingBoard.GetSearchAdvertisingBoard;
+import com.team.back.dto.response.advertisingBoard.GetShortReviewList;
+import com.team.back.dto.response.advertisingBoard.GetUserListAdvertising;
+import com.team.back.dto.response.advertisingBoard.PatchAdvertisingBoard;
+import com.team.back.dto.response.advertisingBoard.PostAdvertisingBoard;
 
 public interface AdvertisingService {
 
   // method : 최신 광고 게시물 리스트 불러오기 메서드 //
-  ResponseEntity<?> getCurrentAdvertisingBoard();  
+  ResponseEntity<? super GetCurrentAdvertisingBoard> getCurrentAdvertisingBoard();  
   // method : 광고 게시물 불러오기 메서드 //
-  ResponseEntity<?> getAdvertisingBoard(Integer boardNumber);
+  ResponseEntity<? super GetAdvertisingboard> getAdvertisingBoard(Integer boardNumber);
   // method : 광고 게시물 검색 리스트 불러오기
-  ResponseEntity<?> getSearchAdvertisingBoard(String searchWord);
+  ResponseEntity<? super GetSearchAdvertisingBoard> getSearchAdvertisingBoard(String searchWord);
   // method : 광고 게시물 한줄 리뷰 리스트 불러오기 
-  ResponseEntity<?> getShortReviewList(Integer boardNumber);
+  ResponseEntity<? super GetShortReviewList> getShortReviewList(Integer boardNumber);
   // method : 특정 유저 게시물 리스트 불러오기 //
-  ResponseEntity<?> getUserListAdvertising(Integer boardNumber);
+  ResponseEntity<? super GetUserListAdvertising> getUserListAdvertising(Integer boardNumber);
   // method : 광고 게시물 작성 메서드//
-  ResponseEntity<?> postAdvertisingBoard(String email, PostAdvertisingRequestDto dto);
+  ResponseEntity<? super PostAdvertisingBoard> postAdvertisingBoard(String email, PostAdvertisingRequestDto dto);
   // method : 광고 게시물 수정 메서드//
-  ResponseEntity<?> patchAdvertisingBoard(Integer boardNumber, String email, PatchAdvertisingRequestDto dto);
+  ResponseEntity<? super PatchAdvertisingBoard> patchAdvertisingBoard(Integer boardNumber, String email, PatchAdvertisingRequestDto dto);
   // method : 광고 게시물 삭제 메서드 //
-  ResponseEntity<?> deleteAdvertisingBoard(Integer boardNumber, String email);
+  ResponseEntity<? super DeleteAdvertisingBoard> deleteAdvertisingBoard(Integer boardNumber, String email);
   // method : 광고 게시물 한줄 리뷰 삭제 메서드 //
-  ResponseEntity<?> deleteShortCommentAdvertisingBoard(Integer boardNumber, String email);
+  ResponseEntity<? super DeleteShortCommentAdvertisingBoard> deleteShortCommentAdvertisingBoard(Integer boardNumber, String email);
 
 
   
