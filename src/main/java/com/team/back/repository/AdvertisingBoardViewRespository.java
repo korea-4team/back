fb0c19cec7e0d44bcfc092357c1c065693528233
CJD0402 <wpehd7547@naver.com>
2023-10-11 17:02:35 +0900

    search

--- src/main/java/com/team/back/repository/AdvertisingBoardViewRespository.java
+++ src/main/java/com/team/back/repository/AdvertisingBoardViewRespository.java
@@ -18,7 +18,7 @@ public interface AdvertisingBoardViewRespository extends JpaRepository<Advertisi
   List<AdvertisingViewEntity> findByLocationOrBusinessTypeOrderByWriteDatetimeDesc(String location, String businessType);
  
   List<AdvertisingViewEntity> findByWriterEmail(String writerEmail);
-  List<AdvertisingViewEntity> findByTitleContainsOrContentsContainsOrLocationOrBusinessTypeOrTagWordOrderByWriteDatetimeDesc(String title, String contents, String location, String businessType, String tagWord);
-  List<AdvertisingViewEntity> findByTitleContainsOrContentsContainsOrBusinessTypeOrTagWordOrderByWriteDatetimeDesc(String title, String contents, String businessType, String tagWord);
+  List<AdvertisingViewEntity> findByTitleContainsOrContentsContainsOrLocationOrBusinessTypeContainsOrTagWordContainsOrderByWriteDatetimeDesc(String title, String contents, String location, String businessType, String tagWord);
+  List<AdvertisingViewEntity> findByTitleContainsOrContentsContainsOrBusinessTypeContainsOrTagWordContainsOrderByWriteDatetimeDesc(String title, String contents, String businessType, String tagWord);
   
 }
